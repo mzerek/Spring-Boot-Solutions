7bf5f3ab2fbf4161cfb630515d7f315fe077be0c
mzerek <maciek.zerek@gmail.com>
2025-09-26 14:14:27 +0200

    Fist commit

---
+++ src/main/java/com/mzerek/springbootsolutions/SpringBootSolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.mzerek.springbootsolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSolutionsApplication.class, args);
+	}
+
+}
